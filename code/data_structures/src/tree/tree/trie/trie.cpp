62863a1a1041e307f1180b2db46b3f228cf51152
shantanu2307 <59186786+shantanu2307@users.noreply.github.com>
2020-05-11 13:04:29 +0530

    Update trie.cpp

--- code/data_structures/src/tree/tree/trie/trie.cpp
+++ code/data_structures/src/tree/tree/trie/trie.cpp
@@ -1,83 +1,72 @@
-#include <cstdio>
-#include <string>
-#include <iostream>
-
-// Part of Cosmos by OpenGenus
+#include<iostream>
+#include<unordered_map>
+#include<array>
 using namespace std;
-#define mod 1000000007
-#define all(v) v.begin(), v.end_()
-#define rep(i, a, b) for (i = (ll)a; i < (ll)b; i++)
-#define revrep(i, a, b) for (i = (ll)a; i >= (ll)b; i--)
-#define strep(it, v) for (it = v.begin(); it != v.end_(); ++it)
-#define ii pair<ll, ll>
-#define MP make_pair
-#define pb push_back
-#define f first
-#define se second
-#define ll long long int
-#define vi vector<ll>
-ll modexp(ll a, ll b)
-{
-    ll res = 1; while (b > 0)
+class node{
+public:
+    char data;//current nodes data
+    bool terminal;// gives the ening of the word
+    unordered_map <char,node *> children;//address of the child nodes;
+    node(char d)
     {
-        if (b & 1)
-            res = (res * a);
-        a = (a * a);  b /= 2;
+        this->data=d;
+        this->terminal=false;
     }
-    return res;
-}
-#define rs resize
-long long readLI()
-{
-    register char c;  for (c = getchar(); !(c >= '0' && c <= '9'); c = getchar())
+};
+class Trie{
+public:
+    node *root;
+    int count;
+    Trie(){
+        this->root=new node('\0');
+        this->count=0;
+
+    }
+    void addword(string str)//adds a word to the trie
     {
+        node *temp=root;
+        for(int i=0;i<str.size();i++){
+            char ch= str[i];
+            if(temp->children.count(ch)){
+                temp=temp->children[ch];
+            }
+            else
+            {
+                node *n=new node(ch);
+                temp->children[ch]=n;
+                temp=n;
+                count++;
+            }
+            temp->terminal=true;
+        }
     }
-    register long long a = c - '0';
-    for (c = getchar(); c >= '0' && c <= '9'; c = getchar())
-        a = (a << 3) + (a << 1) + c - '0';
-    return a;
-}
-const ll N = 100009;
-int n, i, j, sz;
-string a;
-ll next_[27][N], end_[N];
-void add(string a)
-{
-    int v = 0;
-    for (int i = 0; i < (int)a.size(); i++)
+    bool search(string str)//searches a word in the trie
     {
-        int c = a[i] - 'a';
-        if (next_[c][v] == -1)
-            v = next_[c][v] = ++sz;
-        else
-            v = next_[c][v];
+        node *temp=root;
+        for(int i=0;i<str.size();i++){
+            char ch= str[i];
+            if(temp->children.count(ch)){
+                temp=temp->children[ch];
+            }
+            else
+            {
+                return false;
+            }       
+        }
+        return temp->terminal;
     }
-    ++end_[v];
-}
-bool search(string a)
+
+};
+int main(int argc,char const *argv[])
 {
-    int v = 0;
-    for (i = 0; i < (int)a.size(); i++)
+    Trie t;
+    int n;
+    cin>>n;
+    while(n--)
     {
-        int c = a[i] - 'a';
-        if (next_[c][v] == -1)
-            return false;
-        v = next_[c][v];
+        string str;
+        cin>>str;
+        t.addword(str);
     }
-    return end_[v] > 0;
-}
-int main()
-{
-    std::ios_base::sync_with_stdio(false); cin.tie(NULL);
-    cin >> n;
-    rep(i, 0, 27) for (int j = 0; j < N; j++)
-        next_[i][j] = -1;
-    rep(i, 0, n) cin >> a, add(a);
-    cin >> n;
-    while (n--)
-    {
-        cin >> a;
-        cout << search(a) << endl;
-    }
-    return 0;
+    cout<<t.count;
 }
